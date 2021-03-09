29a7a5f59e7c10c248fdfe042f858d047f5cb44e
ebjaime <j.enriquezballe@gmail.com>
2021-03-10 00:09:46 +0100

    Cambio de nombres de programas main de modelos

--- src/models/transe_main.py
+++ src/models/transe_main.py
@@ -24,35 +24,70 @@ from OpenKE.openke.module.loss import MarginLoss
 from OpenKE.openke.module.strategy import NegativeSampling
 from OpenKE.openke.data import TrainDataLoader, TestDataLoader
 
+# =============================================================================
+# PARAMETERS
+# =============================================================================
+
+# Sampling parameters (Train)
+nbatches = 100 # Number of batches when sampling during training
+threads = 8
+bern = 1 # ??? "method" in original script
+filter = 1 # ???
+negative_relations = 0 # Corrupt relations are not needed
+
+# Sampling parameters (Test)
+sampling_mode = "link"
+
+
+# Training parameters
 dataset = "Movielens1M"
+d = 100 # Dimension
 gamma = 1
 lr = 0.001 # Learning rate
-d = 100 # Dimension
 epochs = 1000 
 
+# Normalization parameters
+p_norm = 1 # order of the norm for score
+norm_flag = True # Flag for triple nomralization
 
-# dataloader for training
+
+
+# =============================================================================
+#  DATA LOADERS
+# =============================================================================
+
+# Dataloader for training
 train_dataloader = TrainDataLoader(
 	in_path = "../../data/%s/" % dataset, 
-	nbatches = 100,
-	threads = 8, 
+	nbatches = nbatches,
+	threads = threads, 
 	sampling_mode = "normal", 
-	bern_flag = 1, 
-	filter_flag = 1, 
-	neg_ent = 25,
-	neg_rel = 0)
+	bern_flag = bern, 
+	filter_flag = filter, 
+# 	neg_ent = 25, # Will be declared after calculating batch_size
+	neg_rel = negative_relations)
 
-# dataloader for test
-test_dataloader = TestDataLoader("../../data/%s/" % dataset, "link")
+# Set number of corrupt entities equal to batch size. Each thread will create (batch size / threads)
+train_dataloader.set_ent_neg_rate(train_dataloader.get_batch_size() / threads) 
 
-# define the model
+# Dataloader for test
+test_dataloader = TestDataLoader("../../data/%s/" % dataset, sampling_mode)
+
+# =============================================================================
+#  MODEL
+# =============================================================================
+
 transe = TransE(
 	ent_tot = train_dataloader.get_ent_tot(),
 	rel_tot = train_dataloader.get_rel_tot(),
 	dim = d, 
-	p_norm = 1, 
-	norm_flag = True)
+	p_norm = p_norm, 
+	norm_flag = norm_flag)
 
+
+# =============================================================================
+#  LOSS FUNCTION
+# =============================================================================
 
 # define the loss function
 model = NegativeSampling(
@@ -61,15 +96,21 @@ model = NegativeSampling(
 	batch_size = train_dataloader.get_batch_size()
 )
 
+
+# =============================================================================
+#  TRAINING
+# =============================================================================
 # If the model does not yet exist
 if os.path.isfile('../checkpoint/transe_%s_d%d_e%d_lr%f.ckpt' % (dataset, d, epochs, lr)) is False:    
-    # train the model
+    
     trainer = Trainer(model = model, 
                       data_loader = train_dataloader, 
                       train_times = epochs, 
                       alpha = lr, # Learning rate
                       opt_method="sgd",
                       use_gpu = True)
+    
+    # Train the model
     trainer.run()
     try:
         transe.save_checkpoint('../checkpoint/transe_%s_d%d_e%d_lr%f.ckpt' % (dataset, d, epochs, lr))
@@ -78,6 +119,9 @@ if os.path.isfile('../checkpoint/transe_%s_d%d_e%d_lr%f.ckpt' % (dataset, d, epo
         transe.save_checkpoint('../checkpoint/transe_%s_d%d_e%d_lr%f.ckpt' % (dataset, d, epochs, lr))
     
     
+# =============================================================================
+#  TESTING
+# =============================================================================
 # test the model
 transe.load_checkpoint('../checkpoint/transe_%s_d%d_e%d_lr%f.ckpt' % (dataset, d, epochs, lr))
 tester = Tester(model = transe, data_loader = test_dataloader, use_gpu = True)
